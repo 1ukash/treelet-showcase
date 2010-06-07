d8049675f641b869754e631695b81ddc43e9af15
Alexey A. Lukashin <lukash@ggasoftware.com>
2010-06-07 15:26:13 +0400

    implemnted add/del icons for folders

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,25 +4,38 @@
   */
 (function($) {
 
-  function _renderRootSpan(id, name) {
-    var li = '<li id="' + id + '"><span id="' + id + '" class="ui-tree-root ui-tree-el ui-tree-title">' + name;
-    li += '<span class="ui-add">&nbsp;&nbsp;</span><span class="ui-delete">&nbsp;&nbsp;</span>';
-    li += '</span></li>';
-    var r = $(li);
-    //var s = $(li).children('span.ui-tree-root');
-    r.mouseenter(
+  function _renderRoot(id, name, options, isBase) {
+    var li = $('<li/>').attr('id', id);
+    var span = $('<span class="ui-tree-root ui-tree-el ui-tree-title">' + name + '</span>'). attr('id', id);
+    var add = $('<span class="ui-add">&nbsp;</span>');
+    
+    span.append(add);
+    if (!isBase) {
+      var del = $('<span class="ui-delete">&nbsp;</span>');
+      span.append(del);
+    }
+    
+    li.append(span);
+    span.mouseenter(
       function() {
-        $(this).find("span.ui-add").show();
-        $(this).find("span.ui-delete").show();
+        $(this).find("span.ui-add").css('display','inline-block');
+        $(this).find("span.ui-delete").css('display', 'inline-block');
     }).mouseleave(
       function() {
-        $(this).find("span.ui-add").hide();
-        $(this).find("span.ui-delete").hide();
+        $(this).find("span.ui-add").css('display','none');
+        $(this).find("span.ui-delete").css('display', 'none');
     });
-    return r;
+    return li;
   }
 
-  function _buildFromJson(data, container, options) {
+  function _renderLeaf(id, name) {
+    var li = $('<li/>').attr('id', id);
+    var span = $('<span class="ui-tree-leaf ui-tree-el ui-tree-title">' + name + '</span>'). attr('id', id);
+    li.append(span);
+    return li;
+  }
+
+  function _buildFromJson(data, container, options, isBase) {
     if (data.nodes.length == 0 && data.elements.length == 0) {
       container.append('<ul><li class="ui-nd">no data</li></ul>');
       return;
@@ -31,12 +44,12 @@
 
     $.each(data.nodes, function(i,val) {
       var v = val.node;
-      ul.append(_renderRootSpan(v.id, v.name));
+      ul.append(_renderRoot(v.id, v.name, options, isBase));
     });
 
     $.each(data.elements, function(i,val) {
       var v = val.element;
-      ul.append('<li id="' + v.id + '"><span id="' + v.id + '" class="ui-tree-leaf ui-tree-el ui-tree-title">' + v.name + '</span></li>');
+      ul.append(_renderLeaf(v.id, v.name));
     });
 
     container.append(ul);
@@ -45,14 +58,25 @@
     lis.children('span.ui-tree-root').click(function() {_nodeClickHandler(options, $(this).attr('id'), $(this))});
   }
 
-  function _buildFromHtml(container, options) {
+  function _buildFromHtml(container, options, isBase) {
     container.addClass('ui-tree');
 
     container.find('li').each( function() {
-      $(this).html('<span class="ui-tree-root ui-tree-el ui-tree-title" id="' + $(this).attr('id')  + '">' + $(this).html() + '</span>');
+      var add = '<span class="ui-add">&nbsp;</span>';
+      var del = isBase ? '' : '<span class="ui-delete" >&nbsp;</span>';
+      $(this).html('<span class="ui-tree-root ui-tree-el ui-tree-title" id="' + $(this).attr('id')  + '">' + $(this).html() + add + del + '</span>');
     });
-
-    container.find('li').children('span').click(function() {_nodeClickHandler(options, $(this).attr('id'), $(this))});
+    var span = container.find('li').children('span');
+    span.mouseenter(
+      function() {
+        $(this).find("span.ui-add").css('display','inline-block');
+        $(this).find("span.ui-delete").css('display', 'inline-block');
+    }).mouseleave(
+      function() {
+        $(this).find("span.ui-add").css('display','none');
+        $(this).find("span.ui-delete").css('display', 'none');
+    });
+   span.click(function() {_nodeClickHandler(options, $(this).attr('id'), $(this))});
   }
 
   function _nodeClickHandler(options, pid, el) {
@@ -87,17 +111,21 @@
   }
 
   $.fn.tree = function(callerOptions) {
-    return this.each(function(options) {
+    return this.each(function(callerOptions) {
       var options = $.extend({
         url: '/main/branch',
         nodeParameterName: 'id',
         beforeOpen: null,
         dataType: 'json',
         openError: function(XMLHttpRequest, textStatus, errorThrown) { alert(textStatus) },
-        clickLeaf: function() {alert('leaf! id=' + $(this).attr('id'));}
+        clickLeaf: function() {alert('leaf! id=' + $(this).attr('id'));},
+        treeObject: null
       }, callerOptions || {});
-      _buildFromHtml($(this), options);
-
+      if (options.treeObject == null) { //building from <ul> components
+        _buildFromHtml($(this), options, true);
+      } else { //build from JS object
+        _buildFromJson(options.treeObject, $(this), options);      
+      }
    });
   }
 
